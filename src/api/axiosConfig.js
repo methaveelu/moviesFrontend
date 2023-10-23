f1c0bddd5489965485755b92837ec2f0846e4b04
methaveelu <110245885+methaveelu@users.noreply.github.com>
2023-10-23 18:19:25 +0800

    homePage_and_moviePoster_carousel

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-//configure and export the Axios object that we will use to make HTTP requests to the relevant remote API through this code
+// //configure and export the Axios object that we will use to make HTTP requests to the relevant remote API through this code
 
-export default axios.create({
-    baseURL:"https://9c96-103-106-239-104.ap.ngrok.io/",
-    headers:{"ngrok-skip-browser-warning":"true"}
-    //This setting here is necessary. Because during the development phase, the technology that the remote machine is using to expose the relevant API endpoints is called ngrok.
-    //we need to include this http header setting in order for our client HTTP requests to not be blocked by cors because the relevant web API is running in a different domain, or origin.
-})
+// export default axios.create({
+//     baseURL:"http://localhost:8080",
+//     headers:{"skip-browser-warning":"true"}
+//     //This setting here is necessary. Because during the development phase, the technology that the remote machine is using to expose the relevant API endpoints is called ngrok.
+//     //we need to include this http header setting in order for our client HTTP requests to not be blocked by cors because the relevant web API is running in a different domain, or origin.
+// })
 
