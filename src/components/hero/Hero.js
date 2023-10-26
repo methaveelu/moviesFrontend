d8c2a0ca4a90ee4679dc3bfcceaaeb9f07b09f55
methaveelu <110245885+methaveelu@users.noreply.github.com>
2023-10-26 13:18:27 +0800

    header_hero_trailer

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,17 +2,20 @@ import React from 'react'
 import './Hero.css'
 import Carousel from 'react-material-ui-carousel'//designed to create an image carousel or slider, allowing you to display a series of images or content in a rotating manner.
 import { Paper } from '@mui/material'// gives like a elevation effect to the element.. shadow effect on the borders of component
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 const Hero = ({movies}) => {
   return (
     <div>
         <Carousel>
             {
-                movies?.map((movie, index)=>{
+                movies?.map((movie)=>{
                     return(
                         <Paper key={movie.imdbId}>
                             <div className='movie-card-container' >
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src={movie.poster}/>
@@ -20,8 +23,18 @@ const Hero = ({movies}) => {
                                         <div className='movie-title'>
                                             <h4>{movie.title}</h4>
                                         </div>
+                                        <div className='movie-button-container'>
+                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                <div className='play-button-icon-container'>
+                                                    <FontAwesomeIcon className='play-button-icon' 
+                                                        icon={faCirclePlay}
+                                                    />
+                                                </div>
+                                            </Link>
+                                        </div>
+                                        
                                     </div>
-
+                                    
                                 </div>
                             </div>
                         </Paper>
